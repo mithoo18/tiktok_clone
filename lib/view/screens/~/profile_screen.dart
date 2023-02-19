8915237b788db454e4f535db57c284b2483819df
mithoo18 <devanshu18g@gmail.com>
2023-02-19 14:18:33 +0530

    changes

--- lib/view/screens/~/profile_screen.dart
+++ lib/view/screens/~/profile_screen.dart
@@ -1,1 +1,93 @@
+import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatefulWidget {
+  final String uid;
+  const ProfileScreen({
+    Key? key,
+    required this.uid,
+  }) : super(key: key);
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  final ProfileController profileController = Get.put(profileController());
+
+  @override
+  void initState() {
+    super.initState();
+    profileController.updateUserId(Widget.uid);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<ProfileController>(
+      init : profileController(),
+      builder : (controller){
+        if(controller.user.isEmpty){
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.black12,
+            leading: const Icon(
+              Icons.person_add_alt_1_outlined,
+            ),
+            actions: const [
+              Icon(Icons.more_horiz),
+            ],
+            title: Text(
+              controller.user['name'],
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                color : Colors.white
+              ),
+            ),
+          ),
+          body: SafeArea(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            child: Column(
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    ClipOval(
+                                      child: CachedNetworkImage(
+                                        fit : BoxFit.cover,
+                                        
+                                      ),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        )
+      }
+    )
+  }
+}
